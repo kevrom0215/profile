0f5ce1999b5580cccfdd80f3f6626c5ecee85efc
Kenneth <92443626+kevrom0215@users.noreply.github.com>
2024-09-05 17:52:01 +0800

    added routing and navbar (#8)

---
+++ my-app/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../../styles/global.css'
+
+const Dashboard = () => {
+    return (
+        <div className="wrapper">
+            <h2>Dashboard</h2>
+            <p>Welcome to the Dashboard!</p>
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ my-app/src/components/NavBar/NavBar.js
@@ -0,0 +1,29 @@
+// src/components/Navbar/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import './NavBar.css'; // Import CSS module for styling
+import { logout as authLogout } from '../../services/authService';
+
+const Navbar = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+      authLogout(); // Call the logout function
+      navigate('/logout'); // Redirect to the login page
+    };
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <Link to="/dashboard" className="navbar-link">Dashboard</Link>
+        </li>
+        <li className="navbar-item">
+            <Link to="/logout" onClick={handleLogout} className="navbar-link">Logout</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
