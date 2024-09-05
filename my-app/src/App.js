feee5a74310029187489bba34e5e9970d7f9a282
Kenneth Araga <kennetharaga@gmail.com>
2024-09-05 18:26:39 +0800

    added hashrouting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import LoginForm from './components/LoginForm/LoginForm';
 import Dashboard from './pages/Dashboard/Dashboard';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';  // Ensure these imports
+import { BrowserRouter as Router, Route, Routes, useNavigate, HashRouter } from 'react-router-dom';  // Ensure these imports
 import NotFound from './components/NotFound/NotFound';
 import Navbar from './components/NavBar/NavBar';
 import { login, logout as authLogout, isAuthenticated } from './services/authService'; // Import auth service functions
@@ -42,9 +42,9 @@ const AppContent =()=>{
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <AppContent />
-    </Router>
+    </HashRouter>
   );
 };
 
